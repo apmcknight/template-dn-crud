56a2894d223802475c8aa7d22d30f6b39f204d01
Adam McKnight <adammcknight@me.com>
2020-06-28 15:59:26 -0400

    FIRST COMMIT

---
+++ movie-tracker/models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace movie_tracker.models
+{
+    public class Movie
+    {
+            public int ID { get; set; }
+            public string Title { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime RelaseDate { get; set; }
+            public string Genere { get; set; }
+            public decimal Price { get; set; }
+    }
+}
